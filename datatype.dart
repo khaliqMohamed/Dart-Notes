19cc377313ce75d32c7f60bc2a9f5811b15e19f4
khaliqMohamed <90680495+khaliqMohamed@users.noreply.github.com>
2024-06-03 15:48:57 +0530

    Create datatype.dart

---
+++ datatype.dart
@@ -0,0 +1,25 @@
+ void main(){
+   /*datatypes
+     int
+     double
+     string
+     bool
+     list(same like an array)
+     set*/
+   //variable declaration
+   int number=10
+   print(number);
+   //string value declaration
+   String name='john';
+   print(name);
+
+   //var it's a datatype to automatically assign a datatype depend upon the value. 
+
+   var name='John';
+
+   //by default all datatypes are null and its a object
+
+ }
+   
+     
+      
