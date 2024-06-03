db7abe24fdce74d76a6f228f0cecaa150b1458b5
khaliqMohamed <90680495+khaliqMohamed@users.noreply.github.com>
2024-06-03 14:51:17 +0530

    Create comments.dart

---
+++ comments.dart
@@ -0,0 +1,8 @@
+void main(){
+  //comments used for developers to know what i try to do something on that line
+  //single-line comments
+  /* double-line
+  comments*/
+  ///documentation comments
+  
+}
